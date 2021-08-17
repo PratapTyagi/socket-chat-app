99a35ae76804fb4d50c62f79ffac06a8eb05df11
Pratap Tyagi <tyagipratap111@gmail.com>
2021-08-17 22:24:51 +0530

    Integrated socket api with backend

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import db from "./config/db.js";
+import mongoose from "mongoose";
 import cors from "cors";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const app = express();
+const server = createServer(app);
 
 import messages from "./routes/messages.js";
 import rooms from "./routes/rooms.js";
 import users from "./routes/users.js";
-
-const app = express();
 
 // Middlewares
 app.use(cors());
@@ -21,6 +25,26 @@ app.use("/api/messages", messages);
 app.use("/api/rooms", rooms);
 app.use("/api/users", users);
 
+// Socket setup
+const io = new Server(server);
+io.on("connection", function (socket) {
+  socket.on("disconnect", (reason) => {
+    console.log(reason);
+  });
+});
+
+const database = mongoose.connection;
+
+database.once("open", () => {
+  const msg = database.collection("messagecontents");
+  const changeStream = msg.watch();
+  changeStream.on("change", (change) => {
+    if (change.operationType === "insert") {
+      io.emit("messages", change.fullDocument);
+    }
+  });
+});
+
 // App listening
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
+server.listen(PORT, console.log(`Server running on PORT ${PORT}`));
